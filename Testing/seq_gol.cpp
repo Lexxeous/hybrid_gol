6a3f55e2c4662b7e51392152413afc33a95ea694
Jonathan Alexander Gibson <agibson@gibsonusa.net>
2019-04-05 16:41:44 -0500

    Gens loop, perma-death check, output result
    Created a loop that will repopulate the expanded and next generation matrices repeatedly, based on specified generation value input, implemented a counter to check for full map death (then ceases to continue and outputs at what generation the perma-death occured), and properly output results to <out_file>.

--- Testing/seq_gol.cpp
+++ Testing/seq_gol.cpp
@@ -11,10 +11,11 @@
 using namespace std;
 
 // prototype function(s)
-void pop_def_gol_map(ifstream &init_data, int **def_mat);
-void pop_ext_gol_map(int **def_mat, int def_rows, int def_cols, int **ext_mat);
-int get_def_gol_rows(ifstream &init_data);
-int get_def_gol_cols(ifstream &init_data);
+void pop_org_gol_map(ifstream &init_data, int **org_mat);
+void const_ext_gol_map(int **curr_mat, int org_rows, int org_cols, int **ext_mat);
+int pop_nxt_gol_map(int** ext_mat, int ext_rows, int ext_cols, int **nxt_mat);
+int get_org_gol_rows(ifstream &init_data);
+int get_org_gol_cols(ifstream &init_data);
 
 // start main
 int main(int argc, char* argv[])
@@ -54,35 +55,42 @@ int main(int argc, char* argv[])
   in_file.open(cmd_in_file);
   out_file.open(cmd_out_file);
   
-  int def_gol_rows = get_def_gol_rows(in_file);
-  int def_gol_cols = get_def_gol_cols(in_file);
-  int ext_gol_rows = def_gol_rows + 2;
-  int ext_gol_cols = def_gol_cols + 2;
-
-  cout << "ROWS:COLS " << def_gol_rows << ":" << def_gol_cols << endl;
+  int org_gol_rows = get_org_gol_rows(in_file);
+  int org_gol_cols = get_org_gol_cols(in_file);
+  int ext_gol_rows = org_gol_rows + 2;
+  int ext_gol_cols = org_gol_cols + 2;
 
 
-  int **def_gol_map, **ext_gol_map;
-  def_gol_map = (int**)malloc(def_gol_rows * sizeof(int*));
+  cout << "ROWS:COLS " << org_gol_rows << ":" << org_gol_cols << endl;
+
+
+  // dynamically allocate the original, extended, and next generation matrices
+  int **org_gol_map, **ext_gol_map, **nxt_gol_map;
+  org_gol_map = (int**)malloc(org_gol_rows * sizeof(int*));
   ext_gol_map = (int**)malloc(ext_gol_rows * sizeof(int*));
+  nxt_gol_map = (int**)malloc(org_gol_rows * sizeof(int*));
 
-	for(int d = 0; d < def_gol_rows; d++)
+	for(int d = 0; d < org_gol_rows; d++)
 	{
-		def_gol_map[d] = (int*)malloc(def_gol_cols * sizeof(int));
+		org_gol_map[d] = (int*)malloc(org_gol_cols * sizeof(int));
 	}
-
 	for(int r = 0; r < ext_gol_rows; r++)
 	{
 		ext_gol_map[r] = (int*)malloc(ext_gol_cols * sizeof(int));
 	}
-
-	cout << "Default Game of Life Map (BEFORE population):" << endl;
-	for(int i = 0; i < def_gol_rows; i++)
+	for(int n = 0; n < org_gol_rows; n++)
 	{
-		for(int j = 0; j < def_gol_cols; j++)
+		nxt_gol_map[n] = (int*)malloc(org_gol_cols * sizeof(int));
+	}
+
+
+	cout << "Original Game of Life Map (BEFORE population):" << endl;
+	for(int i = 0; i < org_gol_rows; i++)
+	{
+		for(int j = 0; j < org_gol_cols; j++)
 		{
-			def_gol_map[i][j] = -1;
-			cout << def_gol_map[i][j] << "\t";
+			org_gol_map[i][j] = -1; // initialize the original generation matrix with all -1s
+			cout << org_gol_map[i][j] << "\t";
 		}
 		cout << endl;
 	}
@@ -92,29 +100,40 @@ int main(int argc, char* argv[])
 	{
 		for(int j = 0; j < ext_gol_cols; j++)
 		{
-			ext_gol_map[i][j] = -2;
+			ext_gol_map[i][j] = -2; // initialize the extended generation matrix with all -2s
 			cout << ext_gol_map[i][j] << "\t";
 		}
 		cout << endl;
 	}
 
-
-	pop_def_gol_map(in_file, def_gol_map);
-
-	cout << "Default Game of Life Map (AFTER population):" << endl;
-	for(int i = 0; i < def_gol_rows; i++)
+	cout << "Next Generation Game of Life Map (BEFORE population):" << endl;
+	for(int i = 0; i < org_gol_rows; i++)
 	{
-		for(int j = 0; j < def_gol_cols; j++)
+		for(int j = 0; j < org_gol_cols; j++)
 		{
-			cout << def_gol_map[i][j] << "\t";
+			nxt_gol_map[i][j] = -3; // initialize the next generation matrix with all -3s
+			cout << nxt_gol_map[i][j] << "\t";
 		}
 		cout << endl;
 	}
 
 
-	pop_ext_gol_map(def_gol_map, def_gol_rows, def_gol_cols, ext_gol_map);
+	pop_org_gol_map(in_file, org_gol_map); // populate the original generation matrix with the values given in <in_file>
 
-	cout << "Extended Game of Life Map (AFTER population):" << endl;
+	cout << "Original Game of Life Map (Generation 1):" << endl;
+	for(int i = 0; i < org_gol_rows; i++)
+	{
+		for(int j = 0; j < org_gol_cols; j++)
+		{
+			cout << org_gol_map[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+
+	const_ext_gol_map(org_gol_map, org_gol_rows, org_gol_cols, ext_gol_map); // construct extended generation matrix based on original matrix
+
+	cout << "Extended Game of Life Map (Generation 1):" << endl;
 	for(int i = 0; i < ext_gol_rows; i++)
 	{
 		for(int j = 0; j < ext_gol_cols; j++)
@@ -124,6 +143,67 @@ int main(int argc, char* argv[])
 		cout << endl;
 	}
 
+
+	pop_nxt_gol_map(ext_gol_map, ext_gol_rows, ext_gol_cols, nxt_gol_map);
+
+	cout << "Next Generation Game of Life Map (Generation 1):" << endl;
+	for(int i = 0; i < org_gol_rows; i++)
+	{
+		for(int j = 0; j < org_gol_cols; j++)
+		{
+			cout << nxt_gol_map[i][j] << "\t";
+		}
+		cout << endl;
+	}
+
+	// ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+	for(int g = 2; g <= gens; g++)
+	{
+		const_ext_gol_map(nxt_gol_map, org_gol_rows, org_gol_cols, ext_gol_map); //construct the next extended generation matrix based on current generation matrix
+
+		cout << "Extended Game of Life Map: (Generation " << g << ")" << endl;
+		for(int i = 0; i < ext_gol_rows; i++)
+		{
+			for(int j = 0; j < ext_gol_cols; j++)
+			{
+				cout << ext_gol_map[i][j] << "\t";
+			}
+			cout << endl;
+		}
+
+
+		int dead_cells = pop_nxt_gol_map(ext_gol_map, ext_gol_rows, ext_gol_cols, nxt_gol_map);
+
+		cout << "Next Generation Game of Life Map: (Generation " << g << ")" << endl;
+		for(int i = 0; i < org_gol_rows; i++)
+		{
+			for(int j = 0; j < org_gol_cols; j++)
+			{
+				cout << nxt_gol_map[i][j] << "\t";
+			}
+			cout << endl;
+		}
+
+		if(dead_cells == org_gol_rows * org_gol_cols)
+		{
+			cout << "All cells died at generation " << g << "." << endl;
+			break;
+		}
+	}
+
+
+
+	// after all generations have been run OR everyone dies, write the result(s) to the output file
+	out_file << org_gol_rows << " " << org_gol_cols << endl;
+	for(int i = 0; i < org_gol_rows; i++)
+	{
+		for(int j = 0; j < org_gol_cols; j++)
+		{
+			out_file << nxt_gol_map[i][j] << " ";
+		}
+	}
 
 
   in_file.close();
@@ -134,65 +214,104 @@ int main(int argc, char* argv[])
 
 
 
-void pop_def_gol_map(ifstream &init_data, int **def_mat)
+void pop_org_gol_map(ifstream &init_data, int **org_mat)
 {
-	int def_rows, def_cols, val;
+	int org_rows, org_cols, val;
 
 	if(init_data)
 	{
 		init_data.clear();
 		init_data.seekg(0, ios::beg);
-		init_data >> def_rows >> def_cols;
+		init_data >> org_rows >> org_cols;
 	}
 
-	for(int i = 0; i < def_rows; i++)
+	for(int i = 0; i < org_rows; i++)
 	{
-		for(int j = 0; j < def_cols; j++)
+		for(int j = 0; j < org_cols; j++)
 		{
 			init_data >> val;
-			def_mat[i][j] = val;
+			org_mat[i][j] = val;
 		}
 	}
 }
 
 
-
-void pop_ext_gol_map(int **def_mat, int def_rows, int def_cols, int **ext_mat)
+void const_ext_gol_map(int **curr_mat, int org_rows, int org_cols, int **ext_mat)
 {
 	// populat the corners of the ext_gol_map, in order, clockwise
-	ext_mat[0][0] = def_mat[def_rows-1][def_cols-1]; // first corner
-	ext_mat[0][def_cols+1] = def_mat[def_rows-1][0]; // second corner
-	ext_mat[def_rows+1][def_cols+1] = def_mat[0][0]; // third corner
-	ext_mat[def_rows+1][0] = def_mat[0][def_cols-1]; // fourth corner
+	ext_mat[0][0] = curr_mat[org_rows-1][org_cols-1]; // first corner
+	ext_mat[0][org_cols+1] = curr_mat[org_rows-1][0]; // second corner
+	ext_mat[org_rows+1][org_cols+1] = curr_mat[0][0]; // third corner
+	ext_mat[org_rows+1][0] = curr_mat[0][org_cols-1]; // fourth corner
 
-	// copy all values from "def_gol_map" to the center of "ext_gol_map"
-	for(int i = 0; i < def_rows; i++)
+	// copy all values from "org_gol_map" to the center of "ext_gol_map"
+	for(int i = 0; i < org_rows; i++)
 	{
-		for(int j = 0; j < def_cols; j++)
+		for(int j = 0; j < org_cols; j++)
 		{
-			ext_mat[i+1][j+1] = def_mat[i][j];
+			ext_mat[i+1][j+1] = curr_mat[i][j];
 		}
 	}
 
-	// copy the top and bottom rows of "def_gol_map", swap them, an assign them to "ext_gol_map"
-	for(int j = 0; j < def_cols; j++)
+	// copy the top and bottom rows of "org_gol_map", swap them, an assign them to "ext_gol_map"
+	for(int j = 0; j < org_cols; j++)
 	{
-		ext_mat[0][j+1] = def_mat[def_rows-1][j];
-		ext_mat[def_rows+1][j+1] = def_mat[0][j];
+		ext_mat[0][j+1] = curr_mat[org_rows-1][j];
+		ext_mat[org_rows+1][j+1] = curr_mat[0][j];
 	}
 
-	// copy the left and right cols of "def_gol_map", swap them, an assign them to "ext_gol_map"
-	for(int i = 0; i < def_rows; i++)
+	// copy the left and right cols of "org_gol_map", swap them, an assign them to "ext_gol_map"
+	for(int i = 0; i < org_rows; i++)
 	{
-		ext_mat[i+1][0] = def_mat[i][def_cols-1];
-		ext_mat[i+1][def_cols+1] = def_mat[i][0];
+		ext_mat[i+1][0] = curr_mat[i][org_cols-1];
+		ext_mat[i+1][org_cols+1] = curr_mat[i][0];
 	}
 }
 
 
+int pop_nxt_gol_map(int** ext_mat, int ext_rows, int ext_cols, int **nxt_mat)
+{
+	int curr_sum = 0; // initialize the current neighbor sum
+	int dead_cnt = 0; // counter for how many dead cells will be in the next generation
+
+	for(int i = 1; i <= ext_rows-2; i++)
+	{
+		for(int j = 1; j <= ext_cols-2; j++)
+		{
+			curr_sum = ext_mat[i+1][j] + ext_mat[i-1][j] + ext_mat[i][j+1] + ext_mat[i][j-1] + ext_mat[i-1][j+1] + ext_mat[i+1][j-1] + ext_mat[i+1][j+1] + ext_mat[i-1][j-1];
+
+			if((curr_sum < 2) && (ext_mat[i][j] == 1)) // if live cell has less than 2 neighbors
+			{
+				nxt_mat[i-1][j-1] = 0; // current cell dies, due to under population
+				dead_cnt++;
+			}
+			else if((curr_sum >= 2 && curr_sum <= 3) && (ext_mat[i][j] == 1)) // if live cell has 2~3 neighbors
+			{
+				nxt_mat[i-1][j-1] = 1; // current cell lives, thriving to the next generation
+			}
+			else if(curr_sum > 3 && (ext_mat[i][j] == 1)) // if live cell has more than 3 neighbors
+			{
+				nxt_mat[i-1][j-1] = 0; // current cell dies, due to overpopulation
+				dead_cnt++;
+			}
+			else if(curr_sum == 3 && (ext_mat[i][j] == 0)) // if dead cell has exactly 3 neighbors
+			{
+				nxt_mat[i-1][j-1] = 1; // current cell lives, due to repopulation and migration
+			}
+			else
+			{
+				nxt_mat[i-1][j-1] = 0; // default to dead cell
+				dead_cnt++;
+			}
+
+			curr_sum = 0; // reset the current neighbor sum
+		}
+	}
+	return dead_cnt;
+}
 
 
-int get_def_gol_rows(ifstream &init_data)
+int get_org_gol_rows(ifstream &init_data)
 {
 	int r, c;
 
@@ -206,7 +325,7 @@ int get_def_gol_rows(ifstream &init_data)
 	return r;
 }
 
-int get_def_gol_cols(ifstream &init_data)
+int get_org_gol_cols(ifstream &init_data)
 {
 	int r, c;
 
